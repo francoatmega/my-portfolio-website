b88a86287813d4b9384c0b8be1b3921212bb0cb0
Jardel Matias <jardel.matias@itlean.com.br>
2024-01-16 13:21:37 -0300

    responsive changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,19 +6,20 @@ import { Navigation } from "../components/nav";
 
 export default function about() {
 	return (
-		<div className="h-screen w-1/2 mx-auto flex flex-col items-center justify-center">
-			<div className="flex w-64 inline-block">
+		<div className="h-screen sm:w-4/5 md:w-1/2 mx-auto flex flex-col items-center justify-center">
+			<div className="flex w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 inline-block relative pt-32">
 				<Image
 					src="/author.jpg"
-					width={500}
-					height={500}
 					alt="Picture of the author"
+					fill
+					quality={100}
+					priority
 					className="rounded-full"
 				/>
 			</div>
 			<div className="my-16">
-				<h2 className="text-xl text-justify text-zinc-500 mx-6 text-gray-500">
-				I'm an experienced backend software engineer with a passion for infosec. 
+				<h2 className="text-sm sm:text-base md:text-xl lg:text-2xl text-center text-zinc-500 mx-6 text-gray-500">
+				I'm a backend software engineer with a passion for infosec. 
 				My career took off in 2017, and I've had the opportunity to collaborate with numerous companies on a wide range of projects. 
 				These have encompassed everything from rewriting legacy monolithic applications to fintech products. 
 				I'm a dark theme lover, and my latest pastime involves devouring as many books as I can. 
