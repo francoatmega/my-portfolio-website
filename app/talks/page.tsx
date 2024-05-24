c18568285b3873236e47c40469258509e8c704af
Jardel Matias <joao.j.matias.t_mblabs@grupoboticario.com.br>
2024-05-24 20:30:41 -0300

    my tlaks

--- app/talks/page.tsx
+++ app/talks/page.tsx
@@ -13,54 +13,60 @@ export default function talks() {
 						<Card>
 							<Image
 								src="/images/bsides-sp-19.png"
-								alt="Picture of the author"
+								alt="BSides SP - 19ª Edição"
 								height={200}
 								width={300}
 								className="w-full"
 							/>
 							<div className="flex flex-col gap-4 duration-700 p-4">
-								<h3 className="text-white text-4xl mt-2">Bsidessp19</h3>
-								<p className="text-neutral-400">Eu fui frescar nessa palestra</p>
+								<h3 className="text-white text-4xl mt-2">BSides SP - 19ª Edição</h3>
+								<p className="text-neutral-400">ReDoS for fun and stuck</p>
 								<div className="gap-8 flex">
 									<Link
-										href="/link"
+										href="https://owasp.org/www-chapter-fortaleza/assets/pdfs/ReDoS_BR.pdf"
 										className="duration-200 text-blue-500 hover:text-blue-600"
 									>
 										Link da palestra
-									</Link>
-									<Link
-										href="/link"
-										className="duration-200 text-blue-500 hover:text-blue-600"
-									>
-										Link da video
 									</Link>
 								</div>
 							</div>
 						</Card>
 						<Card>
 							<Image
-								src="/images/bsides-sp-19.png"
+								src="/images/owasp-fortaleza-24-1.png"
 								alt="Picture of the author"
 								height={200}
 								width={300}
 								className="w-full"
 							/>
 							<div className="flex flex-col gap-4 duration-700 p-4">
-								<h3 className="text-white text-4xl mt-2">Bsidessp19</h3>
-								<p className="text-neutral-400">Eu fui frescar nessa palestra</p>
+								<h3 className="text-white text-4xl mt-2">OWASP Chapter Fortaleza 2024</h3>
+								<p className="text-neutral-400">ReDoS for fun and stuck</p>
+								<Link
+										href="https://owasp.org/www-chapter-fortaleza/assets/pdfs/ReDoS_BR.pdf"
+										className="duration-200 text-blue-500 hover:text-blue-600"
+									>
+										Link da palestra
+								</Link>
 							</div>
 						</Card>
 						<Card>
 							<Image
-								src="/images/bsides-sp-19.png"
+								src="/images/hackincariri-pck-2.png"
 								alt="Picture of the author"
 								height={200}
 								width={300}
 								className="w-full"
 							/>
 							<div className="flex flex-col gap-4 duration-700 p-4">
-								<h3 className="text-white text-4xl mt-2">Bsidessp19</h3>
-								<p className="text-neutral-400">Eu fui frescar nessa palestra</p>
+								<h3 className="text-white text-4xl mt-2">Hack In Cariri Pocket - 2ª Edição</h3>
+								<p className="text-neutral-400">Prototype pollution - Da negação de serviço a execução de código remoto no servidor</p>
+								<Link
+										href="https://drive.google.com/file/d/1AIyRFjxg8VyMTpX1Ia0WY89J-GDjA_CQ/view"
+										className="duration-200 text-blue-500 hover:text-blue-600"
+									>
+										Link da palestra
+								</Link>
 							</div>
 						</Card>
 				</div>
